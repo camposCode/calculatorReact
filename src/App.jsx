8c959cf329c2acd4afe663b349092ea9ddd636cb
camposCode <info@josecampos.io>
2022-08-04 15:56:37 +0200

    all functionality done, calculator fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Button from './components/Button/Button'
 import Functions from './components/Functions'
@@ -8,31 +8,33 @@ import Result from './components/Result'
 
 
 const App = () => {
-  const[stack, setStack] = useState('')
-  return (
-    <main className='react-calculator'>
-      <Result value={ stack }/>
-      <Numbers 
-        onClickNumber={number => 
-          setStack(`${stack}${number}`)
-        }
-      />
-      <Functions 
-        onContentClear={() => setStack('')}
-        onDelete={() => 
-          {
-            const newStack = stack.substring(0, stack.length - 1)
-            setStack(newStack)
-          }
-        }
-      />
-      <MathOperations 
-        onClickOperation={ operation => setStack(`${stack}${operation}`)} 
-        
-        onClickEqual={ equal => setStack(`${stack}${equal}`)}
-      />
-    </main>
-  )
+    const [stack, setStack] = useState('')
+    return (
+        <main className='react-calculator'>
+            <Result value={stack}/>
+            <Numbers onClickNumber={
+                number => setStack(`${stack}${number}`)
+            }/>
+            <Functions onContentClear={
+                    () => setStack('')
+                }
+                onDelete={
+                    () => {
+                        if (stack.length > 0) {
+                            const newStack = stack.substring(0, stack.length - 1)
+                            setStack(newStack)
+                        }
+                    }
+                }/>
+            <MathOperations onClickOperation={
+                    operation => setStack(`${stack}${operation}`)
+                }
+
+                onClickEqual={
+                    equal => setStack(eval(stack).toString())
+                }/>
+        </main>
+    )
 }
 
 export default App
